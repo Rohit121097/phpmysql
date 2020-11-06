ed125e5a3a91e1f68657e3cd89c29328523e129b
Rohit121097 <59340086+Rohit121097@users.noreply.github.com>
2020-11-06 11:10:31 +0000

    database name is wrong

--- contact/contact.php
+++ contact/contact.php
@@ -1,7 +1,7 @@
 <?php
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-$con = mysqli_connect('localhost', 'root', '','db_connect');
+$con = mysqli_connect('localhost', 'root', '','db_contact');
 
 // get the post records
 $txtName = $_POST['txtName'];
